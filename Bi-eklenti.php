86a4b65b3bdefc9214192d9dc24dcfe7db1ef977
Bilal <info@bilalsaim.com>
2018-09-06 23:35:00 +0200

    Deneme dosyası eklendi ve Sürüm değiştirildi

--- Bi-eklenti.php
+++ Bi-eklenti.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Bi Eklenti
+Plugin Name: Bi Eklenti Sürüm 2
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
---
+++ Ornek.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Yazılımcı: bilal
+ * Tarih: 06/09/2018
+ * Lisans: MIT
+ */
